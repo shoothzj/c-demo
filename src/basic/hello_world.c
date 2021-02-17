ae567cbb3fe9982e45d787ecb3d44098f52fea84
shoothzj <shoothzj@gmail.com>
2021-02-17 22:52:44 +0800

    Initial commit

---
+++ src/basic/hello_world.c
@@ -0,0 +1,10 @@
+//
+// Created by akkascala on 2021/2/17.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ test/main_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//
+// Created by akkascala on 2021/2/17.
+//
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
